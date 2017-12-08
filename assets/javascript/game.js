c41aa3ec182209a937e47264156db227b871c18e
Patrick Knight <ptknight00@gmail.com>
2017-12-08 15:36:13 -0600

    pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+
+$(document).ready(function() {
+    
+    //Array of Playable Characters
+    let characters = {
+        'charOne': {
+            name: 
+            hp: 
+            attackPower: 
+            imageUrl: 
+            counterAttack: 
+        },
+    };
+        'charTwo': {
+            name: 
+            hp: 
+            attackPower: 
+            imageUrl: 
+            counterAttack: 
+        },
+    };
+        'charThree': {
+            name: 
+            hp: 
+            attackPower: 
+            imageUrl: 
+            counterAttack: 
+        },
+    };
+        'charFour': {
+            name: 
+            hp: 
+            attackPower: 
+            imageUrl: 
+            counterAttack: 
+        },
+    };
+    
+    var charSelected;        //variable for character to play as
+    var activeDef;           //variable for active defender
+    var opponentArray = [];  //array for opponents
+    var charIndex;           //index selected
+    var victories            //opponents defeated
+    var score;               //final game score
+    
+    //render select character screen{
+    // Create function to render game message to DOM
+    //render all characters
+
+    //after char selection
+
+    //render player 1 (change class?)
+    //render combatants(change class?)
+    //render fight screen
+          //move opponents to combatant area
+          //select an combatant to fight
+          //move selected combatant to defense aresa  
+
+        //gameplay
+            //upon attack
+                //display message
+                //change character display to
+                     //change opponent hp
+                     //change player hp, increase attackPower
+                     //check opponent hp
+                         //if opponent defeated
+                         //win condition or new opponent
+                            //play again (reset)
+
+                    //function definitions
+        
+        
+    
