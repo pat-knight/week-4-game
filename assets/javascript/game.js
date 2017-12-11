018b10da4afeb4d8e777e48b5e3e18efa4b02e72
Patrick Knight <ptknight00@gmail.com>
2017-12-11 16:06:10 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,195 @@
 
-$(document).ready(function() {
-    
+
+
+    var players;
+    var game;
+    function gameInit(){
+        console.log("game init");
+         players = playerReset();
+         game = resetGame();
+         renderPlayers();
+    }
     //Array of Playable Characters
-    let characters = {
+    function playerReset() { //render all characters
+       return {
         'charOne': {
-            name: 
-            hp: 
-            attackPower: 
-            imageUrl: 
-            counterAttack: 
+            name: "homer",
+            hp: 120,
+            attackPower: 8,
+            imageUrl: "assets/images/homer.jpg",
+            counterAttack: 15
         },
-    };
         'charTwo': {
-            name: 
-            hp: 
-            attackPower: 
-            imageUrl: 
-            counterAttack: 
+            name: "Grimey",
+            hp: 100,
+            attackPower: 14,
+            imageUrl: "assets/images/frank.jpg",
+            counterAttack: 5
         },
-    };
         'charThree': {
-            name: 
-            hp: 
-            attackPower: 
-            imageUrl: 
-            counterAttack: 
+            name: "Hank",
+            hp: 150,
+            attackPower: 14,
+            imageUrl: "assets/images/hank.jpg",
+            counterAttack: 20
         },
-    };
         'charFour': {
-            name: 
-            hp: 
-            attackPower: 
-            imageUrl: 
-            counterAttack: 
+            name: "McBain",
+            hp: 180,
+            attackPower: 7,
+            imageUrl: "assets/images/mcbain.jpg", 
+            counterAttack: 25
         },
-    };
+    }};
+
+    function resetGame(){
+        return {
+        charSelected: null,
+        activeDef: null,
+        opponentsLeft: 0,
+        attacks: 0
+        }
+    }
+
+    function createPlayerDiv(players, key){
+        console.log("creatCharDiv");
+        var playerDiv = $("<div class='character' data-name='"+ key+"'>");
+        var playerName = $("<div class= 'character-name'>").text(players.name);
+        var playerImage = $("<img alt='player image' class='character-image'>").attr("src", players.imageUrl);
+        var playerHP = $("<div class= 'character-health'>").text(players.hp);
+        playerDiv.append(playerName);
+        playerDiv.append(playerImage);
+        playerDiv.append(playerHP);
+        return playerDiv;
+    }
+
+    function renderPlayers(){
+        console.log("rendering char");
+        var keys = Object.keys(players);
+        for(var i = 0; i< keys.length; i++){
+            var playerKey = keys[i];
+            var player = players[playerKey];
+            var playerDiv = createPlayerDiv(player, playerKey);
+            $('#character-area').append(playerDiv);
+        }
+    }
+
+    function renderOpponents(selectedPlayerKey){
+        console.log("rendering opp");
+        var playerKeys = Object.keys(players)
+        for(var j = 0; j <playerKeys.length; j++){
+                if (playerKeys[j] !== selectedPlayerKey){
+                    var opponentKey = playerKeys[j];
+                    var opponent = players[opponentKey];
+                    var opponentDiv = createPlayerDiv(opponent, opponentKey);
+                    $(opponentDiv).addClass('enemy');
+                    $('#available-to-attack-section').append(opponentDiv);
+                }
+            }
+        }
+
+    function enableOpponentSelection(){
+         $('.enemy').on('click.enemySelect', function(){
+            console.log('opponent selected');
+            var opponent = $(this).attr('data-name');
+            game.activeDef = players[opponent];
+            $('#defender').append(this);
+            $('#attack-button').show();
+            $('.enemy').off('click.enemySelect');
+        }
+    )}
+
+        
+    function attack(attacks){
+        game.activeDef.hp -= game.charSelected.attackPower*attacks;
+        
+    }
+    function defend(){
+        game.charSelected.hp -= game.activeDef.counterAttack;
+    }
+    function checkDefHP(){
+        console.log("checking def HP");
+        return game.activeDef.hp <= 0;
+    }
+    function checkHP(){
+        console.log("check hero hp");
+        return game.charSelected.hp <= 0; 
+    }
+    function gameStatusCheck(){
+        console.log("checking if game won");
+        return game.opponentsLeft === 0;
+    }
+
+    function attackState(){
+        if (checkHP()){
+            alert("You were beaten by "+ game.activeDef.name)
+            $("#selected-character").empty();
+            $("#reset-button").show();
+            return true;
+        } else if (checkDefHP()) {
+            console.log("defender dead");
+            game.opponentsLeft--;
+            $("#defender").empty();
+                if(gameStatusCheck()) {
+                    alert("You Win! Click reset to play again");  
+                    $("#reset-button").show();
+                } else {
+                    alert("You defeated " + game.activeDef.name + "Select another enemy to fight");
+                    enableOpponentSelection();
+                }
+               return true } 
+              return false  }
+        
+        
     
-    var charSelected;        //variable for character to play as
-    var activeDef;           //variable for active defender
-    var opponentArray = [];  //array for opponents
-    var charIndex;           //index selected
-    var victories            //opponents defeated
-    var score;               //final game score
+
+    function emptyDivs(){
+        $('#defender').empty()
+        $('#available-to-attack-section .enemy').empty()
+        $('#character-area').empty()
+        $('#selected-character').empty()
+        $('#characters-section').show()
+    }  
+
+    $(document).ready(function() {
+        $('#character-area').on('click','.character',function(){
+        var selectedKey = $(this).attr('data-name')
+        game.charSelected = players[selectedKey];
+        console.log('player selected')
+        $('#selected-character').append(this)
+        renderOpponents(selectedKey);
+        $('#characters-section').hide()
+        game.opponentsLeft = Object.keys(players).length- 1
+        enableOpponentSelection()})
+        
+        $('#attack-button').on('click.attack',function(){
+             console.log('attack clicked')
+             game.attacks++;
+             attack(game.attacks);
+             defend();
+             $('#selected-character .character-health').text(game.charSelected.hp);
+             $('#defender .character-health').text(game.activeDef.hp);
+             if(attackState()){$(this).hide()}})
+
+        $('#reset-button').on('click.reset',function(){
+            console.log('resetting game')
+            emptyDivs()
+            $(this).hide()
+            gameInit()})
+            gameInit()}) 
+
     
-    //render select character screen{
+    
+    // var charSelected;        //variable for character to play as
+    // var activeDef;           //variable for active defender
+    // var opponentArray = [];  //array for opponents
+    // var charIndex;           //index selected
+    // var victories            //opponents defeated
+    // var score;               //final game score
+    
+    //render select character screen
     // Create function to render game message to DOM
-    //render all characters
+    
 
     //after char selection
 
@@ -69,5 +213,6 @@ $(document).ready(function() {
 
                     //function definitions
         
-        
-    
+
+
+
