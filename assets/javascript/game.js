8035ca5213e2885d6c355149f695991b8e5ae08b
Patrick Knight <ptknight00@gmail.com>
2017-12-14 14:56:32 -0600

    shoring up logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,100 +62,100 @@
     }
 
     function renderPlayers(){
-        var keys = Object.keys(players);
-        for(var i = 0; i< keys.length; i++){
+        var keys = Object.keys(players); //filter player array to select player key
+        for(var i = 0; i< keys.length; i++){// cycle through player-data
             var playerKey = keys[i];
-            var player = players[playerKey];
-            var playerDiv = createPlayerDiv(player, playerKey);
-            $('#character-area').append(playerDiv);
+            var player = players[playerKey]; //create var that holds all player data
+            var playerDiv = createPlayerDiv(player, playerKey);//store in div
+            $('#character-area').append(playerDiv); //fill div
         }
     }
 
-    function renderOpponents(selectedPlayerKey){
+    function displayOpponents(selectedPlayerKey){
         var playerKeys = Object.keys(players)
         for(var j = 0; j <playerKeys.length; j++){
-                if (playerKeys[j] !== selectedPlayerKey){
-                    var opponentKey = playerKeys[j];
-                    var opponent = players[opponentKey];
-                    var opponentDiv = createPlayerDiv(opponent, opponentKey);
-                    $(opponentDiv).addClass('opponent');
-                    $('#available-to-attack-section').append(opponentDiv);
+                if (playerKeys[j] !== selectedPlayerKey){//if player not selected
+                    var opponentKey = playerKeys[j];//add opponent key
+                    var opponent = players[opponentKey];//fill with player data
+                    var opponentDiv = createPlayerDiv(opponent, opponentKey);//create div to fill
+                    $(opponentDiv).addClass('opponent');//adding class here to add clickable function, clear leftover opponent divs after a loss
+                    $('#enemies-on-deck').append(opponentDiv);//fill on deck section with players not selected
                 }
             }
         }
 
     function enableOpponentSelection(){
-         $('.opponent').on('click.enemySelect', function(){
-            var opponent = $(this).attr('data-name');
-            game.activeOpp = players[opponent];
-            $('#opponent').append(this);
-            $('#attack-button').show();
+         $('.opponent').on('click.enemySelect', function(){//when selecting opponent
+            var opponent = $(this).attr('data-name');//store key(data-name) of selected opponent as variable
+            game.activeOpp = players[opponent]; //fill activeOpp with selected opponent data
+            $('#opponent').append(this);//fill opponent id with opponent class
+            $('#attack-button').show(); //display attack button
             $('.opponent').off('click.enemySelect');
         }
     )}
 
         
     function attack(attacks){
-        game.activeOpp.hp -= game.charSelected.attackPower*attacks;      
+        game.activeOpp.hp -= game.charSelected.attackPower*attacks; //reduce enemy damage by increasing amounts based on turn     
     }
 
     function defend(){
-        game.charSelected.hp -= game.activeOpp.counterAttack;
+        game.charSelected.hp -= game.activeOpp.counterAttack;//counter attack selected player
     }
 
     function checkDefHP(){
         console.log("checking def HP");
-        return game.activeOpp.hp <= 0;
+        return game.activeOpp.hp <= 0;//check opponent hp, return if <=0
     }
 
     function checkHP(){
         console.log("check hero hp");
-        return game.charSelected.hp <= 0; 
+        return game.charSelected.hp <= 0; //check player hp, return if <=0
     }
 
     function gameStatusCheck(){
         console.log("checking if game won");
-        return game.opponentsLeft === 0;
+        return game.opponentsLeft === 0;//return if no more enemies to fight
     }
 
     function attackState(){
-        if (checkHP()){
+        if (checkHP()){//selected player dead
             alert("You were beaten by "+ game.activeOpp.name)
             $("#active-player").empty();
             $("#reset-button").show();
             return true;
-        } else if (checkDefHP()) {
+        } else if (checkDefHP()) {//opponent defeated
             game.opponentsLeft--;
             $("#opponent").empty();
-                if(gameStatusCheck()) {
+                if(gameStatusCheck()) {//all opponents defeated
                     alert("You Win! Click reset to play again");  
                     $("#reset-button").show();
-                } else {
+                } else {//opponents remain
                     alert(`You defeated ${game.activeOpp.name} Select your next opponent.`);
                     enableOpponentSelection();
                 }
-               return true } 
-              return false  }
+            return true } //opponent defeated
+        return false  }//opponent not defeated
         
         
     
 
     function clearData(){//clear all created and assigned divs
         $('#opponent').empty();
-        $('#available-to-attack-section .opponent').empty();
+        $('#enemies-on-deck .opponent').empty();
         $('#character-area').empty();
         $('#active-player').empty();
-        // $('.character-section').empty();
-        $('#characters-section').show();
+        $('.opponent').remove();
+        $('#select-player').show();
     }  
 
     $(document).ready(function() {
         $('#character-area').on('click','.player',function(){
-            var selectedKey = $(this).attr('data-name');
-            game.charSelected = players[selectedKey];
-            $('#active-player').append(this);
-            renderOpponents(selectedKey);
-            $('#characters-section').hide();
+            var toPlayer = $(this).attr('data-name');//fill selected player key with data-name
+            game.charSelected = players[toPlayer];//define character selected with selected player
+            $('#active-player').append(this);//fill active-player div with character
+            displayOpponents(toPlayer);//
+            $('#select-player').hide();
             game.opponentsLeft = Object.keys(players).length- 1; 
             enableOpponentSelection();
         }
@@ -175,5 +175,5 @@
         $('#reset-button').on('click.reset',function(){
             clearData();
             $(this).hide();
-            gameInit()})
-            gameInit()}) 
+            gameInit()}) //initialize game after reset
+     gameInit()}) //initialize game on page ready
